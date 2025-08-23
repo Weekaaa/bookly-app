3cf44ed2c10284dce7de8224978e485a085f60c8
Weekaaa <alyalwakeel9@gmail.com>
2025-08-23 17:26:36 +0300

    created text styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
-    fontSize: 20,
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: Colors.white,
+  );
+
+  static final textStyle20 = GoogleFonts.playfairDisplay(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    color: Colors.white,
+  );
+
+  static final textStyle30 = GoogleFonts.playfairDisplay(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
     color: Colors.white,
   );
 }
