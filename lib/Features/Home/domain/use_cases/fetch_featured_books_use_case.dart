aca64ae1266852d7114c4ec8b95973e486a600ae
Weekaaa <alyalwakeel9@gmail.com>
2025-08-25 15:59:20 +0300

    refactored use case

--- lib/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,9 +1,10 @@
 import 'package:bookly_app/Features/Home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Features/Home/domain/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
@@ -11,7 +12,9 @@ class FetchFeaturedBooksUseCase {
   // currently we don't need use cases since we are only fetching the books
   // but we could maybe later add a check for permission or even more use cases
   // which helps in applying single responsibility
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async {
+    // TODO: implement call
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
