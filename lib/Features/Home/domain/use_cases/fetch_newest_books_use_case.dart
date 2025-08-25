786b835837d0c99de1cb572834ef4f1b2c920cd2
Weekaaa <alyalwakeel9@gmail.com>
2025-08-25 16:02:19 +0300

    created fetch newest books use case

---
+++ lib/Features/Home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/Features/Home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/Home/domain/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/use_cases/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, void> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase(this.homeRepo);
+
+  // currently we don't need use cases since we are only fetching the books
+  // but we could maybe later add a check for permission or even more use cases
+  // which helps in applying single responsibility
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async {
+    // TODO: implement call
+    return await homeRepo.fetchNewestBooks();
+  }
+}
